f0f4a89f0094cf4c3e9cea14d0854396ab991f70
natashaadaav <natasha.kolombet@gmail.com>
2020-09-15 02:49:15 +0300

    Updates

---
+++ precache-manifest.447a5e9f338998b75215cf94ae6cefa7.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "95ed0d8520ebf01dfec2ca36e14226f2",
+    "url": "./index.html"
+  },
+  {
+    "revision": "b82686fef3821572ad71",
+    "url": "./static/css/2.86df01db.chunk.css"
+  },
+  {
+    "revision": "b82686fef3821572ad71",
+    "url": "./static/js/2.c0bdb826.chunk.js"
+  },
+  {
+    "revision": "21ced859ea2b2d6b856d461ad6c2afed",
+    "url": "./static/js/2.c0bdb826.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "0f36f9d1ac0ebd4b4be7",
+    "url": "./static/js/main.534db165.chunk.js"
+  },
+  {
+    "revision": "01026e4a03624e34e9a5",
+    "url": "./static/js/runtime-main.21fe6b08.js"
+  }
+]);
